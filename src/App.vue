a0ccbea2846ff9f695b1c11a4dbd1768c16f13ae
AmjadRasheed7 <1211307882@student.mmu.edu.my>
2024-10-06 11:14:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="background">
+    <Card v-for="card in cards" :card="card" :key=card></Card>
+  </div>
+</template>
+
+<script>
+import Card from "./components/Card.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      cards: [
+        {
+          title: "المادية والفردانية",
+          presenter: "عمرو إبراهيم",
+          location: "Location 1",
+          time: "10/24 12:00ص",
+          type: "محاضرة",
+          gender: "الكل",
+          image:"https://woz-u.com/wp-content/uploads/2021/04/woz-what-is-data-mining.jpg",
+        },
+        {
+          title: "مقدمة إلى اللسان الياباني",
+          presenter: "نبيل بن عبداللطيف الصياد",
+          location: "Location 2",
+          time: "10/20 09:00ص",
+          type: "محاضرة",
+          gender: "الكل",
+          image: "https://m.media-amazon.com/images/I/313dtY-LOEL.jpg",
+        },
+        {
+          title: "Power BI لذكاء البيانات",
+          presenter: "سجى السعداوي",
+          location: "Location 3",
+          time: "10/24 10:00ص",
+          type: "محاضرة",
+          gender: "الكل",
+          image: "https://cdn.shopify.com/s/files/1/0899/1470/files/banner-family-hub-peppa-pig_9eeeded8-532c-415d-b536-65277db33261.png",
+        },
+        {
+          title: "تعريف بمزايا دسكورد",
+          presenter: "نبيل بن عبداللطيف الصياد",
+          location: "Location 3",
+          time: "10/24 02:00م",
+          type: "محاضرة",
+          gender: "الكل",
+          image: "https://cdn.shopify.com/s/files/1/0899/1470/files/banner-family-hub-peppa-pig_9eeeded8-532c-415d-b536-65277db33261.png",
+        },
+      ],
+    };
+  },
+  components: {
+    Card,
+  },
+};
+</script>
+
+<style scoped>
+.background {
+  width: 100%;
+  display: grid;
+  place-items: center;
+  padding: 4%;
+  
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,122 @@
+<template>
+  <v-card
+    image="https://images.pexels.com/photos/1509534/pexels-photo-1509534.jpeg?auto=compress&cs=tinysrgb&w=600"
+    class="card mb-9"
+    rounded="lg"
+  >
+    <div class="card-content">
+      <div>
+        <v-img
+          class="image-content"
+          :src="card.image"
+          alt="Card Image"
+          cover
+        ></v-img>
+      </div>
+      <div class="text-content">
+        <h1 class="title">{{ card.title }}</h1>
+        <p class="presenter">
+          <strong>{{ card.presenter }}</strong>
+        </p>
+        <p class="left-text">{{ card.type }}</p>
+        <p class="left-text">{{ card.location }}</p>
+        <p class="left-text">{{ card.time }}</p>
+      </div>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    card: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=El+Messiri:wght@400..700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=El+Messiri&display=swap');
+
+.card {
+  width: 100%;
+  max-width: 450px;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  box-sizing: border-box;
+  border: 4px solid black; /* Increase border width */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.8); /* Add shadow */
+}
+
+.card-content {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+}
+
+.image-content {
+  width: 100%;
+  height: 100%;
+  aspect-ratio: 16 / 9; /* Maintain a 16:9 aspect ratio */
+}
+
+.text-content {
+  height: 100%;
+  padding: 10px;
+  text-align: right;
+  background-color: rgba(
+    248,
+    248,
+    249,
+    0.9
+  ); /* Add a semi-transparent background to make text readable */
+}
+
+.image-container {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to right, #F60862, #F93881); /* Add gradient from white to black */
+}
+
+.title {
+  font-family: "El Messiri", sans-serif;
+  font-optical-sizing: auto;
+  font-size: 2.275em;
+  font-weight: 700;
+  margin-bottom: 0ch;
+  font-style: normal;
+  height: auto
+}
+
+.presenter {
+  font-size: 1.4em;
+  margin-top: 0%;
+  margin-bottom: 2%;
+  font-family: "El Messiri", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 600;
+  font-style: normal;
+  text-align: right;
+
+}
+
+p {
+  font-family: "El Messiri", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 600;
+  font-style: normal;
+  line-height: 1.875;
+  font-size: 1.175em;
+  margin: 0; /* Remove default margin */
+  text-align: right;
+}
+
+</style>
